31c06dc0a02542cf25b5b6a588f9d3c1b6552db1
krl <krl@praerien.dk>
2013-03-15 10:58:08 +0100

    changed module name

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='python-easy-ftp',
+    version='1.0',
+    description='Make it easier to access ftp server content from python. Code can be found here: https://github.com/wilhelmsen/python-easy-ftp',
+    author='Hallgeir Wilhelmsen and colleagues',
+    author_email='wilhelmsen@gmail.com',
+    url='https://github.com/wilhelmsen/python-easy-ftp',
+    py_modules=["easy_ftp",], # Who to build
+)
